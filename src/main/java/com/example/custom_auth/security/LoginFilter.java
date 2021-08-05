b4e84241365f3b0ddeda3b7073123af7f54367b4
Subrat Pani <subrat.pani@airtel.com>
2021-08-05 13:42:14 +0530

    ...

--- src/main/java/com/example/custom_auth/security/LoginFilter.java
+++ src/main/java/com/example/custom_auth/security/LoginFilter.java
@@ -1,23 +1,31 @@
 package com.example.custom_auth.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
-public class AuthFilter extends AbstractAuthenticationProcessingFilter {
+@Slf4j
+@Component
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 
-    protected AuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
-        super(requiresAuthenticationRequestMatcher);
+    protected LoginFilter(RequestMatcher loginURLMatcher) {
+        super(loginURLMatcher);
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        String requestBody = httpServletRequest.getReader().lines().collect(Collectors.joining());
+        log.info("Request Body :  {} ", requestBody);
         return null;
     }
+
 }
--- src/main/java/com/example/custom_auth/security/SecurityConfig.java
+++ src/main/java/com/example/custom_auth/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.example.custom_auth.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,11 +13,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private LoginFilter loginFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .addFilterBefore(loginFilter, LoginFilter.class)
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated().and().authenticationProvider(new CustomAuthenticationProvider());
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
