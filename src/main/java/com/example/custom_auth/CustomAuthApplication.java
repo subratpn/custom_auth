057c27a42c35746b3e942dca9340f2475178b483
Subrat Pani <subrat.pani@airtel.com>
2021-08-05 12:44:53 +0530

    initial commit

---
+++ src/main/java/com/example/custom_auth/CustomAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/custom_auth/CustomAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custom_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
