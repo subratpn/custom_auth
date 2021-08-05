e1192248169572a1a42aa260e5a18b61251c7df4
Subrat Pani <subrat.pani@airtel.com>
2021-08-05 13:28:43 +0530

    ...

---
+++ src/main/java/com/example/custom_auth/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.example.custom_auth.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
